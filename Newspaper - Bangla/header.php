0a45f213a697c2bb614403ff352a65b8ccb94905
Md Abubakar <abubakarbd.info@gmail.com>
2020-09-22 15:22:13 +0600

    first commit

---
+++ Newspaper - Bangla/header.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>বরগুনা পত্রিকা</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="main.css">
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+						<!--- my design Start --->
+						
+		<section class="topbar cler">
+			<div class="templet cler">
+				<div class="topbarleft cler">
+					<ul>
+						<li><a href="#">Sign Up </a></li>
+						<li><a href="#">Login </a></li>
+						<li><a href="#">RSS Feeds </a></li>
+						<li><a href="#">Archived News </a></li>
+					</ul>
+				</div>
+				<div class="topbarright cler"> 
+					<a target="_blank" href="#"><i class="fa fa-facebook"></i></a>
+					<a target="_blank" href="#"><i class="fa fa-twitter"></i></a>
+					<a target="_blank" href="#"><i class="fa fa-linkedin"></i></a>
+					<a target="_blank" href="#"><i class="fa fa-youtube"></i></a>
+				</div>
+			</div>
+		</section>
+		<div class="templet cler">
+			<div class="contentsection cler">
+				<div class="contentsectionleft">
+				<img src="img/18-512.png" alt="লোগো পিকচার"/>
+				<p>সত্তের পথে অবিচল ধির প্রত্যয়</p>
+				</div>
+				
+				<div class="contentsectionright">
+				<img src="img/boi.jpg" alt="বিঙ্গাপন পিকচার"/>
+				</div>
+			</div>
+			
+			<div class="manemanu cler">
+				<ul>
+					<li><a id="activ" href="#">হোম </a></li>
+					<li><a href="#">জাতীয় খবর</a></li>
+					<li><a href="#">আন্তর্জাতিক খবর </a></li>
+					<li><a href="#">সাস্থ বিষয়ক</a></li>
+					<li><a href="#">খেলাদুলা</a></li>
+				</ul>
+			</div>
